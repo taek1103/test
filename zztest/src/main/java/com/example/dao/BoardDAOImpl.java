eaf4009745e71c5df709f3d9f285ab58e04a3aad
BubuZe1a <ee6604@naver.com>
2024-07-08 16:03:21 +0900

    ebg

--- zztest/src/main/java/com/example/dao/BoardDAOImpl.java
+++ zztest/src/main/java/com/example/dao/BoardDAOImpl.java
@@ -1,31 +1,3 @@
 package com.example.dao;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import com.example.domain.BoardVO;
-
-@Repository
-public class BoardDAOImpl {
-	
-	@Autowired
-	private SqlSessionTemplate mybatis;
-	
-	public void insertBoard(BoardVO vo) {
-		mybatis.insert("BoardDAO.insertBoard", vo);
-	}
-	
-	public void updateBoard(BoardVO vo) {
-		mybatis.update("BoardDAO.updateBoard", vo);
-	}
-	
-	public void deleteBoard(BoardVO vo) {
-		mybatis.delete("BoardADO.deleteBoard", vo);
-	}
-	
-	public void incrementViewCount(BoardVO vo) {
-		mybatis.update("BoardDAO.incrementViewCount", vo);
-    }
-
-}
+// git 연동.
